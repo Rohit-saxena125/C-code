f274cbef88c0471a8d70b4b14d762e291d2ec658
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 15:49:28 +0530

    Pyramid

---
+++ Looping/pyramid.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    int n, m;
+    int lines;
+    printf("Enter the numberof lines: ");
+    scanf("%d", &lines);
+    for(n=1; n<=lines; n++)
+    {
+        for(m=1; m<=n; m++)
+            printf("%d", m);
+        printf("\n");    
+    }
+}
