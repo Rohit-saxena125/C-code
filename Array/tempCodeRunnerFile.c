ac5dd983cfca7fd652fff572accd779acd6a3973
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 15:40:48 +0530

    Intial commit

---
+++ Array/tempCodeRunnerFile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void getMaxCountElement(int array[], int size)
+{
+    int i, j, maxCount, maxElement, count;
+    maxCount = INT_MIN;
+    for (i = 0; i < size; i++)
+    {
+        count = 1;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] == array[i])
+            {
+                count++;
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                }
+            }
+        }
+    }
+    printf("%d", maxCount);
+}
+
+int main()
+{
+    int array[1000], i, n;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    getMaxCountElement(array, n);
+
+    return 0;
+}
