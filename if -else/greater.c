208083fad12b7e98ed6c5249453284ccd34330b2
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 15:50:50 +0530

    greater

---
+++ if -else/greater.c
@@ -0,0 +1,13 @@
+// find number which is greater
+#include <stdio.h>
+void main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if (a > b)
+        printf("%d", a - b);
+    else
+    {
+        printf("%d", b - a);
+    }
+}
