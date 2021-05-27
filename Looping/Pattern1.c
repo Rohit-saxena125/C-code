15c4bcd08150ae224f520922a9d9a4eeb414a86c
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-27 16:48:00 +0530

    Initial commit

---
+++ Looping/Pattern1.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+void main()
+{
+    int n, m;
+    int lines;
+    printf("Enter the numberof lines: ");
+    scanf("%d", &lines);
+    for(n=1; n<=lines; n++)
+    {
+        for(m=1; m<=n; m++)
+            printf("%d", m);
+        for(m=n-1; m>0; m--)
+            printf("%d", m);
+        printf("\n");    
+    }
+}
